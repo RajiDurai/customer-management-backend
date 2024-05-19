7722e1dd877b6148245f1163f2e102bb3a945198
RajiDurai <rajalakshmidurai@u.boisestate.edum>
2024-05-18 20:05:07 -0700

    added data and backend logic

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Test");
+const express = require('express'); 
+const customers = require('./customers'); 
+
+// Initialize App 
+const app = express(); 
+
+// Assign route for search
+app.use('/api/customers/search', (req, res, next) => { 
+    const{name} = req.query;
+    const filteredCustomers = customers.filter(
+        (customer) =>
+          customer.firstName.toLowerCase().includes(name.toLowerCase()) ||
+          customer.lastName.toLowerCase().includes(name.toLowerCase())
+      );
+      res.json(filteredCustomers);
+
+}); 
+//Assign route for filter by company name
+app.use('/api/customers/filter', (req, res) => {
+    const { company } = req.query;
+    const filteredCustomers = customers.filter(
+      (customer) => customer.company.toLowerCase() === company.toLowerCase()
+    );
+    res.json(filteredCustomers);
+  });
+
+// Start server on PORT 5000 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ customers.js
@@ -0,0 +1,7 @@
+const customers = [ 
+    { id: 1, firstName: 'Raji', lastName: 'Durai', company: 'NewRelic' },
+    { id: 2, firstName: 'Rachel', lastName: 'Smith', company: 'Google' },
+    { id: 2, firstName: 'Dhara', lastName: 'Thamil', company: 'Google' }
+    ]; 
+    
+    module.exports = customers;
